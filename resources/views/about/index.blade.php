daab50f5515d35af830b8bf19d15cae926a2d2d1
Zin Min Tun <kmailsoftwaredevelopment@gmail.com>
2023-09-26 23:35:17 +0630

    first commit

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+@section('title', 'About of Our Company')
+@section('content')
+  
+@endsection
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,251 @@
+<header class="main-header header-style-one">
+    <div class="header-top">
+        <div class="inner-container">
+            <div class="top-left">
+                <ul class="list-style-one">
+                    <li>
+                        <i class="fa fa-envelope"></i>
+                        <a href="mailto:info@grandwhitestar.com">info@grandwhitestar.com</a>
+                    </li>
+
+                    <li>
+                        <i class="fa fa-mobile-alt"></i>
+                        <a href="tel:(+95) 09 795409789">
+                            (+95) 09 795409789
+                        </a>,
+                        <a href="tel:(+95) 09 451590333">
+                            (+95) 09 451590333
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="top-right">
+                <ul class="useful-links">
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Support</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="outer-box">
+            <ul class="social-icon-one">
+                <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+                <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+                <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="header-lower" style="box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;">
+        <div class="main-box">
+            <div class="logo-box">
+                <div class="logo">
+                    <a href="{{ route('home') }}">
+                        <img src="{{ asset('data/logo.png') }}" alt="" title="Grand White Star Co.,Ltd"
+                            style="width: 130px;">
+                    </a>
+                </div>
+            </div>
+
+            <div class="nav-outer">
+                <nav class="nav main-menu">
+                    <ul class="navigation">
+
+                        <li class="current">
+                            <a href="{{ route('home') }}">
+                                Home
+                            </a>
+                        </li>
+
+                        <li class="current dropdown">
+                            <a href="#">
+                                About Us
+                            </a>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('about.index') }}">
+                                        Our Company
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="{{ route('ceo_message') }}">
+                                        CEO’s MESSAGE
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="{{ route('team') }}">
+                                        Professional Team
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </li>
+
+                        <li class="current">
+                            <a href="{{ asset('data/grand_white_star_profile.pdf') }}" target="_blank">
+                                Company Brochure
+                            </a>
+                        </li>
+
+                        <li class="current dropdown">
+                            <a href="#">
+                                Network & Overseas Jobs
+                            </a>
+                            <ul>
+                                @foreach ($countries as $country)
+                                    <li>
+                                        <a href="{{ route('job.show', $country->id) }}">
+                                            {{ $country->country ?? '' }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+
+
+                        <li class="current dropdown">
+                            <a href="#">
+                                Submit Form
+                            </a>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('employer.index') }}">
+                                        Employer Form
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="{{ route('cv.index') }}">
+                                        Submit Resume
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </li>
+
+                        <li class="current dropdown">
+                            <a href="#">
+                                Gallery & Activities
+                            </a>
+                            <ul>
+                                @foreach ($categories as $category)
+                                    <li>
+                                        <a href="{{ route('activities.show', $category->id) }}">
+                                            {{ $category->title ?? '' }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+
+                        <li class="current">
+                            <a href="{{ route('contact.index') }}">
+                                Contact
+                            </a>
+                        </li>
+
+                    </ul>
+                </nav>
+
+                <div class="outer-box">
+                    <a href="tel:+92(8800)9806" class="info-btn">
+                        <i class="icon fa fa-phone"></i>
+                        <small>Call Anytime</small><br> + 88 ( 9800 ) 6802
+                    </a>
+
+                    <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Mobile Menu  -->
+    <div class="mobile-menu">
+        <div class="menu-backdrop"></div>
+
+        <nav class="menu-box">
+            <div class="upper-box">
+                <div class="nav-logo">
+                    <a href="{{ route('home') }}" title="">
+                        <img src="{{ asset('data/logo.png') }}" alt="" title="">
+                    </a>
+                </div>
+                <div class="close-btn"><i class="icon fa fa-times"></i></div>
+            </div>
+
+            <ul class="navigation clearfix">
+            </ul>
+            <ul class="contact-list-one">
+                <li>
+                    <div class="contact-info-box">
+                        <i class="icon lnr-icon-phone-handset"></i>
+                        <span class="title">Call Now</span>
+                        <a href="tel:(+95) 09 795409789">
+                            (+95) 09 795409789
+                            <br>
+                            (+95) 09 451590333
+                        </a>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="contact-info-box">
+                        <span class="icon lnr-icon-envelope1"></span>
+                        <span class="title">Send Email</span>
+                        <a href="mailto:santintoo@gmail.com">santintoo@gmail.com</a>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="contact-info-box">
+                        <span class="icon lnr-icon-clock"></span>
+                        <span class="title">Send Email</span>
+                        Mon - Sat 8:00 - 5:00, Sunday - CLOSED
+                    </div>
+                </li>
+            </ul>
+
+
+            <ul class="social-links">
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </nav>
+    </div>
+    <!-- End Mobile Menu -->
+
+
+    <!-- Sticky Header  -->
+    <div class="sticky-header">
+        <div class="auto-container">
+            <div class="inner-container">
+
+                <div class="logo">
+                    <a href="{{ route('home') }}" title="">
+                        <img src="{{ asset('data/logo.png') }}" alt="" title="">
+                    </a>
+                </div>
+
+                <div class="nav-outer">
+                    <nav class="main-menu">
+                        <div class="navbar-collapse show collapse clearfix">
+                            <ul class="navigation clearfix">
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <!--Mobile Navigation Toggler-->
+                    <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Sticky Menu -->
+</header>
